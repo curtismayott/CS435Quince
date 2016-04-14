3336c57f77fa5b81a3d0e680d9841610e718f050
Curtis Mayott <cmayott@juneau.cs.colostate.edu>
2016-04-13 22:35:39 -0600

    removed Combiners

--- mapreduce/src/com/cs435/quince/Reducer1.java
+++ mapreduce/src/com/cs435/quince/Reducer1.java
@@ -3,15 +3,22 @@ package com.cs435.quince;
 import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class Reducer1 extends Reducer<Text, Text, Text, Text> {
         public void reduce(Text key, Iterable<Text> values, Context context)
                         throws IOException, InterruptedException {
-                String[] keySplit = key.toString().split("\t");
-                
-		context.write(new Text(), new Text());
-        }
+                String[] coordinates = key.toString().split(" ");
+		double latitude = Double.parseDouble(coordinates[0]);
+		double longitude = Double.parseDouble(coordinates[1]);
+
+		for(Text value : values){
+			String[] tmpValues = value.toString().split(" ");
+			String timeStamp = tmpValue[0];
+			double pmReading = Double.parseDouble(tmpValue[1]);
+		}
+			
+		context.write(new Text(latitude + " " + longitude), new Text());
+	}
 }
