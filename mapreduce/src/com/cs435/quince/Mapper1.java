aaa2f442765bd304cc04242e546eab79c4c3398d
Curtis Mayott <cmayott@juneau.cs.colostate.edu>
2016-04-14 22:27:13 -0600

    Added code to parse the files available at http://aqsdr1.epa.gov/aqsweb/aqstmp/airdata/download_files.html -> Particulates -> 88101

--- mapreduce/src/com/cs435/quince/Mapper1.java
+++ mapreduce/src/com/cs435/quince/Mapper1.java
@@ -9,25 +9,32 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class Mapper1 extends Mapper<LongWritable, Text, Text, Text> {
-	String latitude;
-	String longitude;
-	String pmReading;
+	double latitude;
+	double longitude;
+	double pmReading;
 	String timeStamp;
 
 	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 		String fullText = value.toString();
 		String lines = fullText.split("\n");
 		for(String row : lines){
+			
+			String[] columns = row.split(",");
 			// get latitude
+			latitude = Double.parseDouble(columns[5]);
 
 			// get longitude
+			longitude = Double.parseDouble(columns[6]);
 			
 			// get PM2.5 reading
+			pmReading = Double.parseDouble(columns[16]);
 			
 			// get timestamp
+			timeStamp = columns[11];
+
 			// add oldest year
 			
-			context.write(new Text(latitude + " " + longitude), new Text(timeStamp + " " + pmReading));
+			context.write(new Text(Double.toString(latitude) + " " + Double.toString(longitude)), new Text(timeStamp + " " + Double.toString(pmReading)));
 		}
 	}
 }
