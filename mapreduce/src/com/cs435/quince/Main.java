0d4e1cc7172c2b942ffc566a700a0b07d8d91acf
Curtis Mayott <cmayott@jackson.cs.colostate.edu>
2016-04-25 14:59:14 -0600

    updated Main to handle hdfs

--- mapreduce/src/com/cs435/quince/Main.java
+++ mapreduce/src/com/cs435/quince/Main.java
@@ -22,6 +22,7 @@ public class Main {
 	public static int currentYear = 2016;
 	public static int oldestYear = 0;
 	public static int predictionYear;
+	public static String state;
 	public static void main(String[] args) throws Exception {
 		if (args.length != 6) {
                         System.out.printf("Usage: ProcessLogs <input dir> <number years in future> <latitude start> <latitude end> <longitude start> <longitude end>\n");
@@ -33,8 +34,8 @@ public class Main {
 // args 3 = input dir
 // args 4 = output dir
 
-	        predictionYear = Integer.parseInt(args[0]);
-
+	        predictionYear = Integer.parseInt(args[1]);
+		state = args[2];
 		Configuration conf = new Configuration();
                 boolean success = false;
                 Job job1 = Job.getInstance(conf, "quince1");
@@ -47,8 +48,8 @@ public class Main {
 		//job1.setInputFormatClass(WholeFileInputFormat.class);
 		job1.setMapperClass(Mapper1.class);
 		job1.setReducerClass(Reducer1.class);
-		FileInputFormat.addInputPath(job1, new Path(args[1]));
-		FileOutputFormat.setOutputPath(job1, new Path("/home/output1/"));
+		FileInputFormat.addInputPath(job1, new Path(args[3]));
+		FileOutputFormat.setOutputPath(job1, new Path("/output/"));
 
 		success = job1.waitForCompletion(true);
 		if(success){
@@ -62,10 +63,14 @@ public class Main {
 
                 	job2.setMapperClass(Mapper2.class);
                 	job2.setReducerClass(Reducer2.class);
-			FileInputFormat.addInputPath(job1, new Path("/home/output1/"));
-			FileOutputFormat.setOutputPath(job1, new Path("/home/output2/"));
+			FileInputFormat.addInputPath(job1, new Path("/output/"));
+			FileOutputFormat.setOutputPath(job1, new Path(args[4]));
 			
 			success = job2.waitForCompletion(true);
+			if(!success){
+				System.out.println("Error in Mapper2/Reducer2");
+				System.exit(1);
+			}
 		}else{
 			System.out.println("Error in Mapper1/Reducer1");
 			System.exit(1);
