63c63f5bc28697ef2302eebbc3dd6bcdb281aaf4
Curtis Mayott <cmayott@albany.cs.colostate.edu>
2016-04-26 18:01:40 -0600

    changed sums in Reducer1 to be doubles and not integers

--- mapreduce/src/com/cs435/quince/Reducer1.java
+++ mapreduce/src/com/cs435/quince/Reducer1.java
@@ -13,10 +13,10 @@ public class Reducer1 extends Reducer<Text, Text, Text, Text> {
 		String state = keys[0];
 		String county = keys[1];
 
-		int sumX = 0;
-		int sumXY = 0;
-		int sumY = 0;
-		int sumX2 = 0;
+		double sumX = 0;
+		double sumXY = 0;
+		double sumY = 0;
+		double sumX2 = 0;
 		int size = 0;
 		
 		for(Text value : values){
@@ -34,14 +34,14 @@ public class Reducer1 extends Reducer<Text, Text, Text, Text> {
 		}
 		// a = n * sum(x, y) - sum(x) * sum(y)
 		// 	n * sum(x^2) - sum(x)^2
-		//int a = (size * sumXY - sumX * sumY) / (size * sumX2 - sumX^2);
 		try{
-		int a = ((sumY * sumX2) - (sumX * sumXY)) / ((size * sumX2) - (sumX * sumX));
+		double a = (size * sumXY - sumX * sumY) / (size * sumX2 - sumX * sumX);
+		//int a = ((sumY * sumX2) - (sumX * sumXY)) / ((size * sumX2) - (sumX * sumX));
 
 		// b = (1/n) (sum(y)  - sum(x))
-		//int b = (1 / size) * (sumY - sumX);
-		int b = ((size * sumXY) - (sumX * sumY)) / ((size * sumX2) - (sumX * sumX));
-
+		double b = (1 / size) * (sumY - sumX);
+		//int b = ((size * sumXY) - (sumX * sumY)) / ((size * sumX2) - (sumX * sumX));
+//System.out.println("sumX: " + sumX + " sumY: " + sumY + " sumXY: " + sumXY + " sumX2: " + sumX2 + " " + size);
 		context.write(new Text(state + "\t" + county), new Text(a + "\t" + b));
 		}catch(Exception e){
 			e.printStackTrace();
